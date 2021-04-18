6384cb2fc58ae8079c3ef21660e73126050a794d
ap050609 <71550459+ap050609@users.noreply.github.com>
2021-04-18 15:58:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.position(610, 150);
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('myclownimage_wellnotreally.png');
+}
+
